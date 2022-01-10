84fdba8cba95e6a18f9e0e4b5e3a643b2c31e6a9
bagrodia09 <shubham.bagrodia@knoldus.com>
2022-01-10 21:23:34 +0530

    Uploaded by Shubham

---
+++ src/main/java/com/shubham/kafka/Producer.java
@@ -0,0 +1,29 @@
+package com.shubham.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class Producer {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "localhost:9092");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "com.shubham.kafka.Serialization");
+
+        KafkaProducer<String, User> kafkaProducer = new KafkaProducer<>(properties);
+
+        try {
+            for (int i = 1; i <= 10; i++) {
+                User user = new User(i,"shubham",24,"MCA");
+                kafkaProducer.send(new ProducerRecord("mytopic2", String.valueOf(user.getId()), user));
+                //System.out.println(user);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            kafkaProducer.close();
+        }
+    }
+}
